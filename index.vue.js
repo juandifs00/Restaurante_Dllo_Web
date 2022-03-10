5af9b8c584c1ffaffde8f4e1d5f835604649fcdf
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-03-09 19:44:13 -0600

    - Implementacion VueJS

---
+++ index.vue.js
@@ -0,0 +1,25 @@
+// 1. Identificar el contenedor que va a usar VUE
+// 2. Crear la APP de Vue en el contenedor del paso 1.
+// 3. Crear la estructura del framework vue
+
+const appVue = {
+    // Controlar los componentes del DOM
+    data() {
+        return {
+            mensajeAlerta:"",
+            usuario:{
+                correo:null
+            }
+        }
+    },
+    // Metodos y funciones que voy a utilizar en mi aplicacion
+    methods:  {	
+        obtenerValoresFormulario(){
+            console.log(this.usuario)
+            this.usuario={}
+        }
+    }
+}
+
+
+Vue.createApp(appVue).mount('#app-vue')
